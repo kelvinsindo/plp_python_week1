5efece9717a5d486f273bedbfbcdc66aa54526cf
kelvinsindo <sindokelvin14@gmail.com>
2024-09-20 08:53:33 +0300

    Understanding the concept of user input

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# Prompts to enter name
+name = input("Enter your name: ")
+
+# Prompts to enter age, int func converts the string to integer
+age = int(input("Enter your age: "))
+
+# Prompts to enter location
+location = input("Enter your location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
